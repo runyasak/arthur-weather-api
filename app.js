b27e025a59f0aa907a6d4d073a5a54dfacb81933
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-10-16 15:51:35 +0700

    :pencil: using dotenv in root file

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+/* eslint-disable */
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
+/* eslint-enable */
+
 const express = require('express')
 
 const app = express()
 
-app.set('port', 3000)
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
   res.send('hello weather!')
