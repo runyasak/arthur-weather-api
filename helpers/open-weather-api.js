a8e796b82a26f0c734eca3194bd9b3ff7e0b7819
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-10-17 17:41:13 +0700

    :package: add jest for test

--- helpers/open-weather-api.js
+++ helpers/open-weather-api.js
@@ -1,9 +1,6 @@
 const axios = require('axios')
 
-const instance = axios.create({
-  baseURL: 'http://api.openweathermap.org/data/2.5/weather',
-  timeout: 3000
-})
+const endpointURL = 'http://api.openweathermap.org/data/2.5/weather'
 
 /**
  * Get current weather detail by country id.
@@ -12,7 +9,7 @@ const instance = axios.create({
  */
 
 const currentWeatherByCountryID = async (id) => {
-  const result = await instance.get(`?id=${id}&APPID=${process.env.APP_ID}`)
+  const result = await axios.get(`${endpointURL}?id=${id}&APPID=${process.env.APP_ID}`)
   return result.data
 }
 
@@ -23,7 +20,7 @@ const currentWeatherByCountryID = async (id) => {
  */
 
 const currentWeatherByCountryName = async (name) => {
-  const result = await instance.get(`?q=${name}&APPID=${process.env.APP_ID}`)
+  const result = await axios.get(`${endpointURL}?q=${name}&APPID=${process.env.APP_ID}`)
   return result.data
 }
 
