c8e106e4c4cd0e7f532b07d5f7cdb9356eb961ed
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-10-17 18:36:38 +0700

    :art: restructure test directory

---
+++ __test__/mock/open-weather-api/index.js
@@ -0,0 +1,5 @@
+const successResponse = require('./success-response')
+
+module.exports = {
+  successResponse
+}
---
+++ __test__/unit/current-weather.test.js
@@ -0,0 +1,17 @@
+/* eslint-env jest */
+
+const OpenWeatherAPI = require('../../helpers/open-weather-api')
+const MockOpenWeatherAPI = require('../mock/open-weather-api')
+
+const countryID = 1608132
+
+process.env.APP_ID = 'test123'
+
+describe('#get currentWeatherByCountry ID/Name of Thailand using async/await', () => {
+  it('should get current weather data by ID', async () => {
+    MockOpenWeatherAPI.successResponse.initiate()
+    const result = await OpenWeatherAPI.currentWeatherByCountryID(countryID)
+    expect(result).toBeDefined()
+    expect(result.id).toEqual(countryID)
+  })
+})
