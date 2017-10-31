affe91f6473ae043408391734eca64687f2779e5
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-10-31 14:28:35 +0700

    :heavy_plus_sign: add mapWeatherLog in filter-data and filter only necessary data

--- helpers/filter-data.js
+++ helpers/filter-data.js
@@ -1,7 +1,27 @@
 /**
- * Get filtered data from props
+ * Map key of weather log data
  * @param {object} data
- * @param {...array} props
+ * @returns {array}
+ */
+
+const mapWeatherLog = data =>
+  data.reduce(
+    (current, value) => [
+      ...current,
+      {
+        weather_code: value.code,
+        weather_data: value.date,
+        weather_high: value.high,
+        weather_low: value.low,
+        weather_text: value.text
+      }
+    ],
+    []
+  )
+
+/**
+ * Get filtered data for necessary use
+ * @param {object} data
  * @returns {object}
  */
 
@@ -9,8 +29,8 @@ exports.filter = data =>
   Object.assign(
     {},
     {
-      created: data.query.created,
-      condition: data.query.results.channel.item.condition,
-      forecast: data.query.results.channel.item.forecast
+      surccess: true,
+      current_condition: data.query.results.channel.item.condition,
+      weather_log: mapWeatherLog(data.query.results.channel.item.forecast)
     }
   )
